6f5c87fe7e0a2f087ae6f3f0957c62a411578d51
Francis M. Tyers <ftyers@prompsit.com>
2011-06-04 12:08:25 +0000

    new regression test script

--- pending-tests.sh
+++ pending-tests.sh
@@ -1,1 +1,1 @@
-python regtest.py -d . cv-ru http://wiki.apertium.org/wiki/Special:Export/Chuvash_and_Russian/Pending_tests
+aq-regtest -d . cv-ru http://wiki.apertium.org/wiki/Special:Export/Chuvash_and_Russian/Pending_tests
