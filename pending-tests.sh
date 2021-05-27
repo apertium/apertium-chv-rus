545cdd8efe727fecb6243baeda7ff89eae3d6e77
Tino Didriksen <mail@tinodidriksen.com>
2021-05-27 11:34:32 +0200

    OFTC, http -> https, url fixups

--- pending-tests.sh
+++ pending-tests.sh
@@ -1,3 +1,3 @@
 hash aq-regtest 2>&- || { echo >&2 "I require 'aq-regtest' but it's not installed.  Please install apertium-quality."; exit 1; }
 
-aq-regtest -d . cv-ru http://wiki.apertium.org/wiki/Special:Export/Chuvash_and_Russian/Pending_tests
+aq-regtest -d . cv-ru https://wiki.apertium.org/wiki/Special:Export/Chuvash_and_Russian/Pending_tests
